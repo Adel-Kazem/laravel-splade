24788d80f1e75396912152ea6beacc6dd20a227a
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2024-09-19 07:55:20 +0300

    Update radios.blade.php
    Daisy

--- resources/views/form/radios.blade.php
+++ resources/views/form/radios.blade.php
@@ -1,7 +1,11 @@
 <x-splade-component is="group" :name="$name" :inline="$inline" :help="$help" {{ $attributes }}>
-    <x-slot:label>{{ $label }}</x-slot:label>
+    <x-slot:label>{{ $label }}
+        @isset($afterLabel)
+            <span class="inline-block"> {{ $afterLabel }} </span>
+        @endisset
+    </x-slot:label>
 
     @foreach($options as $value => $label)
-        <x-splade-component is="radio" :name="$name" :label="$label" :value="$value" />
+        <x-splade-component is="radio" :name="$name" :label="$label" :value="$value"/>
     @endforeach
 </x-splade-component>
