3d4f6ffab5f66971fd2f250b650a57704f4a1ff5
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2024-09-19 07:45:26 +0300

    Update checkbox.blade.php
    Daisy

--- resources/views/form/checkbox.blade.php
+++ resources/views/form/checkbox.blade.php
@@ -1,24 +1,35 @@
+@props([
+    'name',
+    'value' => 1,
+    'falseValue' => 0,
+    'label' => '',
+    'help' => false,
+    'showErrors' => true,
+    'afterLabel' => ''
+])
+
 <div {{ $attributes->only(['v-if', 'v-show', 'class']) }}>
     <label class="flex items-center">
-        <input {{ $attributes->except(['v-if', 'v-show', 'class'])->class(
-            'rounded border-gray-300 text-indigo-600 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 disabled:opacity-50'
-        )->merge([
+        <input {{ $attributes->except(['v-if', 'v-show', 'class'])->class('checkbox checkbox-sm')->merge([
             'name' => $name,
-            'value' => $value,
             'type' => 'checkbox',
             'v-model' => $vueModel(),
             'data-validation-key' => $validationKey(),
-        ]) }} :true-value="@js($value)" :false-value="@js($falseValue)" />
+        ]) }} :true-value="{{ $value }}" :false-value="@js($falseValue)" />
 
         @if(trim($slot))
             <span class="ml-2">{{ $slot }}</span>
         @else
             <span class="ml-2">{{ $label }}</span>
         @endif
+
+        @isset($afterLabel)
+            <div>
+                {{ $afterLabel }}
+            </div>
+        @endisset
     </label>
 
     @includeWhen($help, 'splade::form.help', ['help' => $help])
     @includeWhen($showErrors, 'splade::form.error', ['name' => $validationKey()])
 </div>
-
-
