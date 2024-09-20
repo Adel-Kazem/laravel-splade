08940e66305774f43cb7c191638190f8a5494106
Adel-Kazem <75915359+Adel-Kazem@users.noreply.github.com>
2024-09-20 08:07:55 +0300

    Update submit.blade.php
    2nd update of classes

--- resources/views/form/submit.blade.php
+++ resources/views/form/submit.blade.php
@@ -50,28 +50,20 @@
         $buttonClasses .= ' w-full';
     }
 
-    // Merge the component's classes with any additional classes passed to it
+    // Merge custom classes with the component's classes
     $mergedClasses = $attributes->get('class') ? $buttonClasses . ' ' . $attributes->get('class') : $buttonClasses;
 @endphp
 
-<div>
-    <button 
-        id="{{ $id }}" 
-        class="{{ $mergedClasses }}"
-        {{ $attributes->except(['class'])->merge([
-            'type' => 'submit',
-        ])->when($name, fn($attr) => $attr->merge(['name' => $name, 'value' => $value])) }}
-    >
+<div @class($wrapperClass)>
+    <button id="{{ $id }}" class="{{ $mergedClasses }}" {{ $attributes->except('class')->merge([
+    ])->when($name, fn($attr) => $attr->merge(['name' => $name, 'value' => $value])) }}>
         @if(trim($slot))
             {{ $slot }}
         @else
             <div class="flex flex-row items-center justify-center">
                 <svg
-                    v-if="@js($spinner) && form.processing"
-                    class="animate-spin mr-3 h-4 @if($type === 'white') text-gray-700 @else text-white @endif" 
-                    xmlns="http://www.w3.org/2000/svg" 
-                    fill="none" 
-                    viewBox="0 0 24 24"
+                        v-if="@js($spinner) && form.processing"
+                        class="animate-spin mr-3 h-4 @if($type === 'white') text-gray-700 @else text-white @endif" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                 >
                     <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"/>
                     <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"/>
